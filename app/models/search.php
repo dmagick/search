3fe74d452f48be4520c18a037e07f15d339702af
Chris <dmagick@gmail.com>
2011-11-03 22:38:48 +1100

    get the search model to actually make a request

--- app/models/search.php
+++ app/models/search.php
@@ -17,18 +17,24 @@ class searchModel
      * Keep a reference to the config so we only load it up once.
      * This is passed to all models - they can deal with it as they wish.
      */
-    protected static $_config = array();
+    private static $_config = array();
+
+    /**
+     * Api url. This should probably be in the config, but for now we'll set it here.
+     */
+    private static $_flickrApiUrl = 'http://api.flickr.com/services/rest/';
 
     private function __construct()
     {
     }
 
-    public function getInstance($config=array())
+    public static function getInstance($config=array())
     {
         if (self::$_instance === NULL) {
-            $class = __CLASS__;
-            self::$_instance = new $class;
-            self::$_instance->config = $config;
+            $class              = __CLASS__;
+            $instance           = new $class;
+            $instance::$_config = $config;
+            self::$_instance    = $instance;
         }
         return self::$_instance;
     }
@@ -42,15 +48,11 @@ class searchModel
      * they may be.
      * It's up to the controller or function calling this to deal with things.
      */
-    public static function search($searchTerms=array(), $page=-1)
+    public static function search($searchTerms='', $page=-1)
     {
 
         if (empty($searchTerms) === TRUE) {
             return FALSE;
-        }
-
-        if (is_array($searchTerms) === FALSE) {
-            $searchTerms = explode(' ', $searchTerms);
         }
 
         /**
@@ -63,6 +65,85 @@ class searchModel
         if ($pageNum < 1) {
             $pageNum = 1;
         }
+
+        $instance = self::$_instance;
+        $config   = $instance::$_config;
+
+        $apiRequest  = self::$_flickrApiUrl.'?method=flickr.photos.search&';
+        $apiRequest .= 'api_key='.$config['flickrApiKey'].'&';
+        $apiRequest .= 'per_page='.$config['perpage'].'&';
+        $apiRequest .= 'page='.$pageNum.'&';
+        $apiRequest .= 'text='.str_replace(' ', '+', $searchTerms);
+
+        $errorNum = -1;
+        $errorStr = NULL;
+        // Curl gives better errors, so we'll try to use that.
+        if (function_exists('curl_init') === TRUE) {
+            $curl = curl_init($apiRequest);
+            curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+            $response = curl_exec($curl);
+            $info     = curl_getinfo($curl);
+            $errorNum = curl_errno($curl);
+            $errorStr = curl_error($curl);
+            curl_close($curl);
+        } else {
+            $response = file_get_contents($apiRequest);
+        }
+
+        if ($response === FALSE) {
+            throw new Exception("Unable to get url: ".$apiRequest." . Error: ".$errorStr." (".$errorNum.")");
+        }
+
+        $responseXml = simplexml_load_string($response);
+
+        // See http://www.flickr.com/services/api/flickr.photos.search.html
+
+        $responseAttributes = $responseXml->attributes();
+        $responseType       = $responseAttributes['stat'];
+
+        $searchResults = array();
+        $searchResults['response'] = (string)$responseType;
+
+        if ($responseType === 'fail') {
+            foreach ($responseXml->children() as $error) {
+                $errorInfo = $error->attributes();
+                $errorCode = $errorInfo['code'];
+                $errorMsg  = $errorInfo['msg'];
+            }
+            $searchResults['info'] = array(
+                                      'code'    => $errorCode,
+                                      'message' => $errorMsg,
+                                     );
+            return $searchResults;
+        }
+
+        foreach ($responseXml->children() as $photoInfo) {
+            $attributes = $photoInfo->attributes();
+            $searchResults['info'] = array(
+                                      'pages' => (int)$attributes['pages'],
+                                      'total' => (int)$attributes['total'],
+                                     );
+
+            $searchResults['photos'] = array();
+            foreach ($photoInfo->children() as $photo) {
+                $attributes = $photo->attributes();
+
+                $title = (string)$attributes['title'];
+
+                // See http://www.flickr.com/services/api/misc.urls.html
+                $baseUrl  = 'http://farm'.$attributes['farm'].'.static.flickr.com/';
+                $baseUrl .= $attributes['server'].'/';
+                $baseUrl .= $attributes['id'].'_';
+                $thumbUrl = $baseUrl.$attributes['secret'].'_m.jpg';
+                $fullUrl  = $baseUrl.$attributes['secret'].'_b.jpg';
+                $searchResults['photos'][] = array(
+                                              'thumbnail' => $thumbUrl,
+                                              'fullurl'   => $fullUrl,
+                                              'title'     => $title,
+                                             );
+            }
+        }
+        return $searchResults;
     }
 }
 
